dd71cfce144bd0dae854940cdaf30b1ed3c9cafe
Miro Svrtan <miro@mirosvrtan.me>
2012-06-26 22:16:05 +0200

    Added test1 user class

---
+++ test1/User.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Simple user class implementation for purpose of testing differences between class implementations
+ * - using public properties
+ */
+class User
+{
+
+    public $userId;
+
+    public $userName;
+
+    public $userPassword;
+
+    public $userRegistrationTs;
+
+    public $userActivationDateTime;
+
+}
+
+?>
