eed0eb2a62f39f0374ba323ed3201ab4f3acc352
Miro Svrtan <miro@mirosvrtan.me>
2012-06-26 22:17:45 +0200

    Added shell script for running 10000 initializations per each test of testObjectInitialization.php

---
+++ runTestObjectInitialization.sh
@@ -0,0 +1,5 @@
+php testObjectInitialization.php -t 1 -i 10000
+php testObjectInitialization.php -t 2 -i 10000
+php testObjectInitialization.php -t 3 -i 10000
+php testObjectInitialization.php -t 4 -i 10000
+php testObjectInitialization.php -t 5 -i 10000
